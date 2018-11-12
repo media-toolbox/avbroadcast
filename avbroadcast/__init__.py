d45355f0a6b9a67f9efb10a668f64d3ac451f9aa
Andreas Motl <andreas.motl@elmyra.de>
2018-11-12 20:11:16 +0100

    Bump version: 0.1.0 → 0.2.0

--- avbroadcast/__init__.py
+++ avbroadcast/__init__.py
@@ -1,2 +1,2 @@
 """avbroadcast - republish media streams for mass consumption"""
-__version__ = '0.1.0'
+__version__ = '0.2.0'
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 
 setup(name='avbroadcast',
-      version='0.1.0',
+      version='0.2.0',
       description='avbroadcast - republish media streams for mass consumption',
       long_description=README,
       license="AGPL 3, EUPL 1.2",
